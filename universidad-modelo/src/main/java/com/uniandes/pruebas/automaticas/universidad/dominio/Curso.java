28d7ed3cce21e3c311f4336800e69898dec20914
dtmaciasca <dt.macias@uniandes.edu.co>
2020-03-27 23:05:48 -0500

    first commit

---
+++ universidad-modelo/src/main/java/com/uniandes/pruebas/automaticas/universidad/dominio/Curso.java
@@ -0,0 +1,43 @@
+package com.uniandes.pruebas.automaticas.universidad.dominio;
+
+import java.util.List;
+
+public class Curso {
+
+    private String nombre;
+    private Integer creditos;
+    private String codigo;
+    private List<Estudiante> estudiantes;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getCreditos() {
+        return creditos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCreditos(Integer creditos) {
+        this.creditos = creditos;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEstudiantes(List<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+}
